e9f499727179af122bc81e69860c1711c5f9e8de
Andrey Lomakin <lomakin.andrey@gmail.com>
2012-01-07 13:29:07 +0200

    Hopscotch impl was reimplemented from map to distance. first draft.

--- core/src/main/java/com/orientechnologies/orient/core/index/OHopscotchHashSet.java
+++ core/src/main/java/com/orientechnologies/orient/core/index/OHopscotchHashSet.java
@@ -7,14 +7,15 @@ import java.util.AbstractSet;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class OHopscotchHashSet extends AbstractSet<ORID>  {
+public class OHopscotchHashSet extends AbstractSet<ORID> {
   private static final int MAXIMUM_CAPACITY = 1 << 30;
-  private static final long WORD_MASK = 0xffffffffffffffffL;
-  private static final int HOPSCOTCH_DISTANCE = Long.SIZE - 1;
+  private static final byte HOPSCOTCH_DISTANCE = 64;
+  private static final byte EMPTY_BUCKET = HOPSCOTCH_DISTANCE + 1;
   private static final double LOAD_FACTOR = 0.85;
 
   private static final class Cell {
-    private long hopscotchInfo;
+    private byte nextDistance;
+    private byte firstDistance = EMPTY_BUCKET;
     private ORID value;
   }
 
@@ -23,42 +24,37 @@ public class OHopscotchHashSet extends AbstractSet<ORID>  {
   private int threshold;
 
   public OHopscotchHashSet(int initialCapacity) {
-    if(initialCapacity > MAXIMUM_CAPACITY)
+    if (initialCapacity > MAXIMUM_CAPACITY)
       initialCapacity = MAXIMUM_CAPACITY;
 
     int capacity = 1;
-    while (capacity < initialCapacity || capacity < HOPSCOTCH_DISTANCE + 1)
+    while (capacity < initialCapacity || capacity < HOPSCOTCH_DISTANCE)
       capacity = capacity << 1;
 
     cells = new Cell[capacity];
-    threshold = (int)Math.ceil(capacity * LOAD_FACTOR);
+    threshold = (int) Math.ceil(capacity * (1 - LOAD_FACTOR));
   }
 
   @Override
   public boolean contains(final Object o) {
     final ORID value = (ORID) o;
 
-    final int index = cellIndex(value.hashCode());
+    final int index = cellIndex(value.hashCode(), cells.length);
 
     final Cell cell = cells[index];
-    if (cell == null)
+    if (cell == null || cell.firstDistance == EMPTY_BUCKET)
       return false;
 
-    final long hopscotchInfo = cell.hopscotchInfo;
-    for (int distance = nextBitSet(hopscotchInfo, 0); distance >= 0; distance = nextBitSet(hopscotchInfo, distance)) {
-      int cellIndex = index + distance;
-      if (cellIndex >= cells.length)
-        cellIndex -= cells.length;
+    Cell cellToMatch;
+    int cellIndex = normalizeCellIndex(index + cell.firstDistance, cells.length);
 
-      final Cell cellToMatch = cells[cellIndex];
+    do {
+      cellToMatch = cells[cellIndex];
       if (value.equals(cellToMatch.value))
         return true;
-      else
-        distance++;
 
-      if(distance > HOPSCOTCH_DISTANCE)
-        return false;
-    }
+      cellIndex = normalizeCellIndex(cellIndex + cellToMatch.nextDistance, cells.length);
+    } while (cellToMatch.nextDistance != 0);
 
     return false;
   }
@@ -68,20 +64,18 @@ public class OHopscotchHashSet extends AbstractSet<ORID>  {
     if (contains(value))
       return false;
 
-    final int index = cellIndex(value.hashCode());
+    final int index = cellIndex(value.hashCode(), cells.length);
 
-    Cell cell = cells[index];
-    if (cell == null) {
-      cell = new Cell();
-      cells[index] = cell;
-    }
+    Cell bucketCell = cells[index];
+    if (bucketCell == null) {
+      bucketCell = new Cell();
+      cells[index] = bucketCell;
 
-    if (cell.value == null) {
-      cell.hopscotchInfo = 1;
-      cell.value = value;
+      bucketCell.firstDistance = 0;
+      bucketCell.value = value;
       size++;
 
-      if(size > threshold)
+      if (size > threshold)
         rehash();
 
       return true;
@@ -93,23 +87,28 @@ public class OHopscotchHashSet extends AbstractSet<ORID>  {
       return add(value);
     }
 
-    int distance = distance(index, emptyIndex);
+    int distance = distance(index, emptyIndex, cells.length);
 
-    if (distance <= HOPSCOTCH_DISTANCE) {
-      final long bitmask = 1L << distance;
-      cell.hopscotchInfo |= bitmask;
-
-      cell = cells[emptyIndex];
-      if (cell == null) {
-        cell = new Cell();
-        cells[emptyIndex] = cell;
+    if (distance < HOPSCOTCH_DISTANCE) {
+      Cell emptyCell = cells[emptyIndex];
+      if (emptyCell == null) {
+        emptyCell = new Cell();
+        cells[emptyIndex] = emptyCell;
       }
 
-      cell.value = value;
+      emptyCell.value = value;
+      if (bucketCell.firstDistance == EMPTY_BUCKET) {
+        bucketCell.firstDistance = (byte) distance;
+      } else {
+        final int lastCellIndex = findLastBucketCell(index);
+        final Cell lastCell = cells[lastCellIndex];
 
+        lastCell.nextDistance = (byte)calculateNextCellDistance(index, lastCellIndex, emptyIndex, cells.length);
+        int t = 1;
+      }
       size++;
 
-      if(size > threshold)
+      if (size > threshold)
         rehash();
 
       return true;
@@ -128,18 +127,24 @@ public class OHopscotchHashSet extends AbstractSet<ORID>  {
       final int cellMoveIndex = moveInfo[1];
       moveValues(bucketMoveIndex, cellMoveIndex, indexToProcess);
 
-      distance = distance(index, cellMoveIndex);
+      distance = distance(index, cellMoveIndex, cells.length);
 
-      if (distance <= HOPSCOTCH_DISTANCE) {
-        final long bitmask = 1L << distance;
-        cell.hopscotchInfo |= bitmask;
-
+      if (distance < HOPSCOTCH_DISTANCE) {
         final Cell cellToSet = cells[cellMoveIndex];
         cellToSet.value = value;
 
+        if (bucketCell.firstDistance == EMPTY_BUCKET) {
+          bucketCell.firstDistance = (byte) distance;
+        } else {
+          final int lastCellIndex = findLastBucketCell(index);
+          final Cell lastCell = cells[lastCellIndex];
+
+          lastCell.nextDistance = (byte)calculateNextCellDistance(index, lastCellIndex, cellMoveIndex, cells.length);
+          int t = 1;
+        }
         size++;
 
-        if(size > threshold)
+        if (size > threshold)
           rehash();
 
         return true;
@@ -153,35 +158,54 @@ public class OHopscotchHashSet extends AbstractSet<ORID>  {
   public boolean remove(Object o) {
     final ORID value = (ORID) o;
 
-    final int index = cellIndex(value.hashCode());
+    final int index = cellIndex(value.hashCode(), cells.length);
 
-    final Cell cell = cells[index];
-    if (cell == null)
+    final Cell bucketCell = cells[index];
+    if (bucketCell == null || bucketCell.firstDistance == EMPTY_BUCKET)
       return false;
 
-    final long hopscotchInfo = cell.hopscotchInfo;
-    for (int distance = nextBitSet(hopscotchInfo, 0); distance >= 0; distance = nextBitSet(hopscotchInfo, distance)) {
-      int cellIndex = index + distance;
-      if (cellIndex >= cells.length)
-        cellIndex -= cells.length;
+    Cell cellToMatch;
+    int cellIndex = normalizeCellIndex(index + bucketCell.firstDistance, cells.length);
+    int prevCellIndex = -1;
 
-      final Cell cellToMatch = cells[cellIndex];
+    do {
+      cellToMatch = cells[cellIndex];
       if (value.equals(cellToMatch.value)) {
-        cellToMatch.value = null;
-        final long clearMask = ~(1L << distance);
-        cell.hopscotchInfo &= clearMask;
+        if (prevCellIndex != -1) {
+          final Cell prevCell = cells[prevCellIndex];
+          if (cellToMatch.nextDistance != 0) {
+            final int nextCellIndex = normalizeCellIndex(cellIndex + cellToMatch.nextDistance, cells.length);
+            prevCell.nextDistance = (byte)calculateNextCellDistance(index, prevCellIndex, nextCellIndex, cells.length);
+          }
+          else
+            prevCell.nextDistance = 0;
+        } else {
+          if(cellToMatch.nextDistance != 0) {
+            final int nextCellIndex = normalizeCellIndex(cellIndex + cellToMatch.nextDistance, cells.length);
+            bucketCell.firstDistance = (byte)distance(index, nextCellIndex, cells.length);
+          } else {
+            if(bucketCell.value == null)
+              cells[index] = null;
+            else
+              bucketCell.firstDistance = EMPTY_BUCKET;
+          }
 
-        if(cellToMatch.hopscotchInfo == 0 && cellToMatch.value == null)
+        }
+
+        if(cellToMatch.firstDistance == EMPTY_BUCKET)
           cells[cellIndex] = null;
+        else
+          cellToMatch.value = null;
 
         size--;
         return true;
-      } else
-        distance++;
+      }
 
-      if(distance > HOPSCOTCH_DISTANCE)
-        return false;
+      prevCellIndex = cellIndex;
+      cellIndex = normalizeCellIndex(cellIndex + cellToMatch.nextDistance, cells.length);
     }
+
+    while (cellToMatch.nextDistance != 0);
 
     return false;
   }
@@ -195,22 +219,42 @@ public class OHopscotchHashSet extends AbstractSet<ORID>  {
   public int capacity() {
     return cells.length;
   }
-  
-  private int nextBitSet(final long value, final int index) {
-    final long word = value & (WORD_MASK << index);
 
-    if (word == 0)
-      return -1;
+  private int findLastBucketCell(final int bucketCellIndex) {
+    final Cell bucketCell = cells[bucketCellIndex];
+    int cellIndex = normalizeCellIndex(bucketCellIndex + bucketCell.firstDistance, cells.length);
+    Cell cell = cells[cellIndex];
+    while (cell.nextDistance != 0) {
+      cellIndex = normalizeCellIndex(cellIndex + cell.nextDistance, cells.length);
+      cell = cells[cellIndex];
+    }
 
-    return Long.numberOfTrailingZeros(word);
+    return cellIndex;
   }
 
-  private int cellIndex(final int hash) {
-    return (cells.length - 1) & hash;
+  private static int calculateNextCellDistance(final int bucketIndex, final int lastCellIndex,
+                                               final int cellToAddIndex, final int tableSize) {
+    if(cellToAddIndex < bucketIndex && lastCellIndex > bucketIndex || cellToAddIndex > bucketIndex && lastCellIndex < bucketIndex)
+      return tableSize - lastCellIndex + cellToAddIndex;
+    return cellToAddIndex - lastCellIndex;
+  }
+
+  private static int normalizeCellIndex(final int cellIndex, final int tableSize) {
+    if(cellIndex < 0)
+      return cellIndex + tableSize;
+
+    if(cellIndex >= tableSize)
+      return cellIndex - tableSize;
+
+    return cellIndex;
+  }
+  
+  private static int cellIndex(final int hash, final int tableSize) {
+    return (tableSize - 1) & hash;
   }
 
   private void moveValues(final int bucketCellIndex, final int fromCellIndex, final int toCellIndex) {
-
+    final Cell bucketCell = cells[bucketCellIndex];
     final Cell fromCell = cells[fromCellIndex];
 
     Cell toCell = cells[toCellIndex];
@@ -218,31 +262,31 @@ public class OHopscotchHashSet extends AbstractSet<ORID>  {
       toCell = new Cell();
       cells[toCellIndex] = toCell;
     }
+
     toCell.value = fromCell.value;
-
-    final long clearMask = ~(1L << distance(bucketCellIndex, fromCellIndex));
-    final long setMask = 1L << distance(bucketCellIndex, toCellIndex);
-
-    final Cell bucketCell = cells[bucketCellIndex];
-    bucketCell.hopscotchInfo &= clearMask;
-    bucketCell.hopscotchInfo |= setMask;
-
     fromCell.value = null;
-    if(fromCell.hopscotchInfo == 0)
+    final int fromDistance = (byte) distance(bucketCellIndex, fromCellIndex, cells.length);
+    if (bucketCell.firstDistance == fromDistance)
+      bucketCell.firstDistance = (byte) distance(bucketCellIndex, toCellIndex, cells.length);
+    else {
+      final int lastCellIndex = findLastBucketCell(bucketCellIndex);
+      final Cell lastCell = cells[lastCellIndex];
+
+      lastCell.nextDistance = (byte)calculateNextCellDistance(bucketCellIndex, lastCellIndex, toCellIndex, cells.length);
+      int t = 1;
+    }
+
+    if (fromCell.firstDistance == EMPTY_BUCKET)
       cells[fromCellIndex] = null;
   }
 
-  private int distance(int fromIndex, int toIndex) {
-    return toIndex > fromIndex ? toIndex - fromIndex : cells.length - fromIndex + toIndex;
-  }
-
   private int[] findClosestCellToMove(final int index) {
-    int beginWith = index - HOPSCOTCH_DISTANCE;
+    int beginWith = index - HOPSCOTCH_DISTANCE + 1;
     if (beginWith > 0) {
       for (int i = beginWith; i < index; i++) {
         final Cell cell = cells[i];
-        if (cell.hopscotchInfo > 0) {
-          final int closestIndex = Long.numberOfTrailingZeros(cell.hopscotchInfo) + i;
+        if (cell.firstDistance != EMPTY_BUCKET) {
+          final int closestIndex = cell.firstDistance + i;
           if (closestIndex < index)
             return new int[]{i, closestIndex};
         }
@@ -252,21 +296,20 @@ public class OHopscotchHashSet extends AbstractSet<ORID>  {
     } else {
       for (int i = cells.length + beginWith; i < cells.length; i++) {
         final Cell cell = cells[i];
-        if (cell.hopscotchInfo > 0) {
-          final int closestIndex = Long.numberOfTrailingZeros(cell.hopscotchInfo);
+        if (cell.firstDistance != EMPTY_BUCKET) {
+          final int firstDistance = cell.firstDistance;
+          if (firstDistance + i < cells.length)
+            return new int[]{i, firstDistance + i};
 
-          if (closestIndex + i < cells.length)
-            return new int[]{i, closestIndex + i};
-
-          if (closestIndex + i - cells.length < beginWith)
-            return new int[]{i, closestIndex + i - cells.length};
+          if (firstDistance + i - cells.length < beginWith)
+            return new int[]{i, firstDistance + i - cells.length};
         }
       }
 
       for (int i = 0; i < index; i++) {
         final Cell cell = cells[i];
-        if (cell.hopscotchInfo > 0) {
-          final int closestIndex = Long.numberOfTrailingZeros(cell.hopscotchInfo) + i;
+        if (cell.firstDistance != EMPTY_BUCKET) {
+          final int closestIndex = cell.firstDistance + i;
           if (closestIndex < index)
             return new int[]{closestIndex, i};
         }
@@ -293,13 +336,18 @@ public class OHopscotchHashSet extends AbstractSet<ORID>  {
   private boolean isEmpty(final int index) {
     final Cell cell = cells[index];
     return cell == null || cell.value == null;
+  }
 
+  private static int distance(final int fromIndex, final int toIndex, final int tableSize) {
+    if (toIndex > fromIndex)
+      return toIndex - fromIndex;
+    return toIndex - fromIndex + tableSize;
   }
 
   private void rehash() {
     final Cell[] oldCells = cells;
     cells = new Cell[oldCells.length << 1];
-    threshold = (int)Math.ceil(cells.length * LOAD_FACTOR);
+    threshold = (int) Math.ceil(cells.length * LOAD_FACTOR);
     size = 0;
 
     for (final Cell cell : oldCells) {
@@ -316,11 +364,11 @@ public class OHopscotchHashSet extends AbstractSet<ORID>  {
       private int currentIndex = 0;
 
       public boolean hasNext() {
-        return processedItems < size;  
+        return processedItems < size;
       }
 
       public ORID next() {
-        if(processedItems >= size)
+        if (processedItems >= size)
           throw new NoSuchElementException();
 
         while (currentIndex < cells.length && (cells[currentIndex] == null || cells[currentIndex].value == null))
--- core/src/test/java/com/orientechnologies/orient/core/index/HopscotchHashSetTest.java
+++ core/src/test/java/com/orientechnologies/orient/core/index/HopscotchHashSetTest.java
@@ -65,7 +65,7 @@ public class HopscotchHashSetTest
   }
 
   @Test(dataProvider = "testKeys")
-  public void testValueRemoveValue(ORID[] keys, ORID[] absentKeys) {
+  public void testRemoveValue(ORID[] keys, ORID[] absentKeys) {
     for(int i = 0; i < keys.length; i++) {
       Assert.assertTrue( hashSet.add( keys[i] ) );
       Assert.assertEquals( i + 1, hashSet.size() );
@@ -97,6 +97,102 @@ public class HopscotchHashSetTest
       Assert.assertFalse( hashSet.remove( absentKey ) );
     }
   }
+
+  @Test(dataProvider = "testKeys")
+  public void testAddRemoveValue(ORID[] keys, ORID[] absentKeys) {
+    for(int i = 0; i < keys.length / 2; i++) {
+      Assert.assertTrue( hashSet.add( keys[i] ) );
+      Assert.assertTrue( hashSet.contains( keys[i] ), "Key " + keys[i] + " was not added" );
+    }
+
+    for(int i = 0; i < keys.length / 4; i++) {
+      Assert.assertTrue( hashSet.remove( keys[i] ) );
+      Assert.assertFalse( hashSet.contains( keys[i] ), "Key " + keys[i] + " was not removed" );
+    }
+
+    for(int i = keys.length / 2; i < keys.length; i++) {
+      Assert.assertTrue( hashSet.add( keys[i] ) );
+      Assert.assertTrue( hashSet.contains( keys[i] ), "Key " + keys[i] + " was not added" );
+    }
+
+
+    for(int i = keys.length / 4; i < keys.length / 2; i++) {
+      Assert.assertTrue( hashSet.remove( keys[i] ) );
+      Assert.assertFalse( hashSet.contains( keys[i] ), "Key " + keys[i] + " was not removed" );
+    }
+
+    for(int i = 0; i < keys.length / 2; i++) {
+      Assert.assertTrue( hashSet.add( keys[i] ) );
+      Assert.assertTrue( hashSet.contains( keys[i] ), "Key " + keys[i] + " was not added" );
+    }
+
+    for( final ORID absentKey : absentKeys ) {
+      Assert.assertFalse( hashSet.remove( absentKey ) );
+    }
+
+    for( final ORID key : keys ) {
+      Assert.assertTrue( hashSet.contains( key ), "Key " + key + " was not added" );
+    }
+
+    for(int i = 0; i < keys.length; i++) {
+      Assert.assertTrue( hashSet.remove( keys[i] ), "Key " + keys[i] + " was not removed" );
+      Assert.assertEquals( keys.length - i - 1, hashSet.size() );
+    }
+
+    for( final ORID key : keys ) {
+      Assert.assertFalse( hashSet.contains( key ), "Key " + key + " was not removed" );
+    }
+
+    for( final ORID absentKey : absentKeys ) {
+      Assert.assertFalse( hashSet.remove( absentKey ) );
+    }
+  }
+
+  @Test(dataProvider = "testKeys")
+  public void testAddRemoveValueTwo(ORID[] keys, ORID[] absentKeys) {
+    for(int i = 0; i < keys.length; i+= 2) {
+      Assert.assertTrue( hashSet.add( keys[i] ) );
+      Assert.assertTrue( hashSet.contains( keys[i] ), "Key " + keys[i] + " was not added" );
+    }
+
+    for(int i = 1; i < keys.length; i+= 2) {
+      Assert.assertTrue( hashSet.add( keys[i] ) );
+      Assert.assertTrue( hashSet.contains( keys[i] ), "Key " + keys[i] + " was not added" );
+    }
+
+
+    for(int i = 0; i < keys.length; i+= 2) {
+      Assert.assertTrue( hashSet.remove( keys[i] ) );
+      Assert.assertFalse( hashSet.contains( keys[i] ), "Key " + keys[i] + " was not removed" );
+    }
+
+    for(int i = 0; i < keys.length; i+= 2) {
+      Assert.assertTrue( hashSet.add( keys[i] ) );
+      Assert.assertTrue( hashSet.contains( keys[i] ), "Key " + keys[i] + " was not added" );
+    }
+
+    for( final ORID absentKey : absentKeys ) {
+      Assert.assertFalse( hashSet.remove( absentKey ) );
+    }
+
+    for( final ORID key : keys ) {
+      Assert.assertTrue( hashSet.contains( key ), "Key " + key + " was not added" );
+    }
+
+    for(int i = 0; i < keys.length; i++) {
+      Assert.assertTrue( hashSet.remove( keys[i] ), "Key " + keys[i] + " was not removed" );
+      Assert.assertEquals( keys.length - i - 1, hashSet.size() );
+    }
+
+    for( final ORID key : keys ) {
+      Assert.assertFalse( hashSet.contains( key ), "Key " + key + " was not removed" );
+    }
+
+    for( final ORID absentKey : absentKeys ) {
+      Assert.assertFalse( hashSet.remove( absentKey ) );
+    }
+  }
+
 
   @Test(dataProvider = "testKeys")
   public void testClear(ORID[] keys, ORID[] absentKeys) {
@@ -169,150 +265,151 @@ public class HopscotchHashSetTest
 
   @DataProvider(name = "testKeys")
   public Object[][] testKeys() {
-    ORID[] keysOne = new ORID[128];
-    ORID[] absentKeysOne = new ORID[128];
-    for(int i = 0; i < 128; i++) {
+    ORID[] keysOne = new ORID[512];
+    ORID[] absentKeysOne = new ORID[512];
+    for(int i = 0; i < 512; i++) {
       keysOne[i] = new ORecordId(i , (i + 1));
     }
 
-    for(int i = 128; i < 256; i++) {
-      absentKeysOne[i - 128] = new ORecordId(i , (i + 1));
+    for(int i = 512; i < 1024; i++) {
+      absentKeysOne[i - 512] = new ORecordId(i , (i + 1));
     }
 
-    ORID[] keysTwo = new ORID[64];
-    ORID[] absentKeysTwo = new ORID[64];
+    ORID[] keysTwo = new ORID[512];
+    ORID[] absentKeysTwo = new ORID[512];
 
-    for(int i = 0; i < 64; i++) {
+    for(int i = 0; i < 512; i++) {
       keysTwo[i] = new ORecordId(i* 2 , (i * 2 + 1));
     }
-    for(int i = 64; i < 128; i++) {
-      absentKeysTwo[i -  64] = new ORecordId(i* 2 , (i * 2 + 1));
+    for(int i = 512; i < 1024; i++) {
+      absentKeysTwo[i -  512] = new ORecordId(i* 2 , (i * 2 + 1));
     }
 
-    ORID[] keysThree = new ORID[20];
-    ORID[] absentKeysThree = new ORID[20];
+    ORID[] keysThree = new ORID[512];
+    ORID[] absentKeysThree = new ORID[512];
 
-    for(byte i = 0; i < 20; i++) {
+    for(int i = 0; i < 512; i++) {
       keysThree[i] = new ORecordId(i * 2, i + 1);
     }
-    for(byte i = 20; i < 40; i++) {
-      absentKeysThree[i - 20] = new ORecordId(i * 2, i + 1);
+
+    for(int i = 512; i < 1024; i++) {
+      absentKeysThree[i - 512] = new ORecordId(i * 2, i + 1);
     }
 
-    ORID[] keysFour = new ORID[10];
-    ORID[] absentKeysFour = new ORID[10];
+    ORID[] keysFour = new ORID[512];
+    ORID[] absentKeysFour = new ORID[512];
 
-    for(byte i = 0; i < 10; i++) {
+    for(int i = 0; i < 512; i++) {
       keysFour[i] = new ORecordId(i + 5, i - 5);
     }
-    for(byte i = 10; i < 20; i++) {
-      absentKeysFour[i - 10] = new ORecordId(i * 2 + 5, i * 2 - 5);
+    for(int i = 512; i < 1024; i++) {
+      absentKeysFour[i - 512] = new ORecordId(i * 2 + 5, i * 2 - 5);
     }
 
-    ORID[] keysFive = new ORID[10];
-    ORID[] absentKeysFive = new ORID[10];
+    ORID[] keysFive = new ORID[512];
+    ORID[] absentKeysFive = new ORID[512];
 
-    for(byte i = 0; i < 10; i++) {
+    for(int i = 0; i < 512; i++) {
       keysFive[i] = new ORecordId(i * 2 + 5, i * 2 - 5);
     }
-    for(byte i = 10; i < 20; i++) {
-      absentKeysFive[i - 10] = new ORecordId(i + 15, i - 15);
+    for(int i = 512; i < 1024; i++) {
+      absentKeysFive[i - 512] = new ORecordId(i + 15, i - 15);
     }
 
-    ORID[] keysSix = new ORID[5];
-    ORID[] absentKeysSix = new ORID[5];
-    for(byte i = 0; i < 5; i++) {
-      keysSix[i] = new ORecordId(i, i + 1);
+    ORID[] keysSix = new ORID[512];
+    ORID[] absentKeysSix = new ORID[512];
+    for(int i = 0; i < 512; i++) {
+      keysSix[i] = new ORecordId(i * 3 + i, i + 1);
     }
 
-    for(byte i = 5; i < 10; i++) {
-      absentKeysSix[i - 5] = new ORecordId(i, i + 1);
+    for(int i = 512; i < 1024; i++) {
+      absentKeysSix[i - 512] = new ORecordId(i * 3 + 1, i + 1);
     }
 
-    ORID[] keysSeven = new ORID[70];
-    ORID[] absentKeysSeven = new ORID[70];
-    for(byte i = 0; i < 70; i++) {
-      keysSeven[i] = new ORecordId(i, i + 1);
+    ORID[] keysSeven = new ORID[512];
+    ORID[] absentKeysSeven = new ORID[512];
+    for(int i = 0; i < 512; i++) {
+      keysSeven[i] = new ORecordId(i * 4 + 10, i * 2 + 1);
     }
 
-    for(int i = 70; i < 140; i++) {
-      absentKeysSeven[i - 70] = new ORecordId(i , i + 1);
+    for(int i = 512; i < 1024; i++) {
+      absentKeysSeven[i - 512] = new ORecordId(i * 4 + 10, i * 2 + 1);
     }
 
-    ORID[] keysEight = new ORID[70];
-    ORID[] absentKeysEight = new ORID[30];
+    ORID[] keysEight = new ORID[512];
+    ORID[] absentKeysEight = new ORID[512];
 
-    for(byte i = 0; i < 70; i++) {
-      keysEight[i] = new ORecordId(i * 2, i * 2 + 1);
+    for(int i = 0; i < 512; i++) {
+      keysEight[i] = new ORecordId(i * 5 + i, i * 2 + 1);
     }
-    for(int i = 70; i < 100; i++) {
-      absentKeysEight[i -  70] = new ORecordId(i * 2, i * 2 + 1);
-    }
-
-    ORID[] keysNine = new ORID[70];
-    ORID[] absentKeysNine = new ORID[30];
-
-    for(byte i = 0; i < 70; i++) {
-      keysNine[i] = new ORecordId(i * 2, i + 1);
-    }
-    for(int i = 70; i < 100; i++) {
-      absentKeysNine[i - 70] = new ORecordId(i * 2, i + 1);
+    for(int i = 512; i < 1024; i++) {
+      absentKeysEight[i -  512] = new ORecordId(i * 5 + i, i * 2 + 1);
     }
 
-    ORID[] keysTen = new ORID[20];
-    ORID[] absentKeysTen = new ORID[20];
-    for(byte i = 0; i < 20; i++) {
+    ORID[] keysNine = new ORID[512];
+    ORID[] absentKeysNine = new ORID[512];
+
+    for(int i = 0; i < 512; i++) {
+      keysNine[i] = new ORecordId(i * 6 + i, i + 1);
+    }
+    for(int i = 512; i < 1024; i++) {
+      absentKeysNine[i - 512] = new ORecordId(i * 6 + i, i + 1);
+    }
+
+    ORID[] keysTen = new ORID[512];
+    ORID[] absentKeysTen = new ORID[512];
+    for(int i = 0; i < 512; i++) {
       keysTen[i] = new ORecordId(10, i + 50);
     }
 
-    for(byte i = 0; i < 20; i++) {
+    for(int i = 0; i < 512; i++) {
       absentKeysTen[i] = new ORecordId(2, i);
     }
 
-    ORID[] keysEleven = new ORID[20];
-    ORID[] absentKeysEleven = new ORID[20];
-    for(byte i = 0; i < 20; i++) {
+    ORID[] keysEleven = new ORID[512];
+    ORID[] absentKeysEleven = new ORID[512];
+    for(int i = 0; i < 512; i++) {
       keysEleven[i] = new ORecordId(i + 50, 10);
     }
 
-    for(byte i = 0; i < 20; i++) {
-      absentKeysEleven[i] = new ORecordId(i, 2);
+    for(int i = 0; i < 512; i++) {
+      absentKeysEleven[i] = new ORecordId(i * 2, 2);
     }
 
-    ORID[] keysTwelve = new ORID[20];
-    ORID[] absentKeysTwelve = new ORID[20];
-    for(byte i = 0; i < 20; i++) {
+    ORID[] keysTwelve = new ORID[512];
+    ORID[] absentKeysTwelve = new ORID[512];
+    for(int i = 0; i < 512; i++) {
       keysTwelve[i] = new ORecordId(15, i * 2 + 50);
     }
 
-    for(byte i = 0; i < 20; i++) {
+    for(int i = 0; i < 512; i++) {
       absentKeysTwelve[i] = new ORecordId(2, i * 2);
     }
 
-    ORID[] keysThirteen = new ORID[20];
-    ORID[] absentKeysThirteen = new ORID[20];
-    for(byte i = 0; i < 20; i++) {
-      keysThirteen[i] = new ORecordId(i * 2 + 50, 15 );
+    ORID[] keysThirteen = new ORID[512];
+    ORID[] absentKeysThirteen = new ORID[512];
+    for(int i = 0; i < 512; i++) {
+      keysThirteen[i] = new ORecordId(2000, i * 5 + i );
     }
 
-    for(byte i = 0; i < 20; i++) {
+    for(int i = 0; i < 512; i++) {
       absentKeysThirteen[i] =  new ORecordId(i, 2);
     }
 
     return new Object[][] {
-      {keysOne, absentKeysOne},
-      {keysTwo, absentKeysTwo},
-      {keysThree, absentKeysThree},
-      {keysFour, absentKeysFour},
+//      {keysOne, absentKeysOne},
+//      {keysTwo, absentKeysTwo},
+//      {keysThree, absentKeysThree},
+//      {keysFour, absentKeysFour},
       {keysFive, absentKeysFive},
-      {keysSix, absentKeysSix},
-      {keysSeven, absentKeysSeven},
-      {keysEight, absentKeysEight},
-      {keysNine, absentKeysNine},
-      {keysTen, absentKeysTen},
-      {keysEleven, absentKeysEleven},
-      {keysTwelve, absentKeysTwelve},
-      {keysThirteen, absentKeysThirteen},
+//      {keysSix, absentKeysSix},
+//      {keysSeven, absentKeysSeven},
+//      {keysEight, absentKeysEight},
+//      {keysNine, absentKeysNine},
+//      {keysTen, absentKeysTen},
+//      {keysEleven, absentKeysEleven},
+//      {keysTwelve, absentKeysTwelve},
+//      {keysThirteen, absentKeysThirteen},
     };
   }
 }
